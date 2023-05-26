4d9d225a689e992735b907ff2e516847e163bae7
l.mzhachikh <l.mzhachikh@corp.mail.ru>
2023-05-26 15:11:50 +0300

    Fix ts error: remove display: swap prop of font

--- public/fonts/FiraCode/fira-code.ts
+++ public/fonts/FiraCode/fira-code.ts
@@ -5,27 +5,22 @@ export const firaCode = localFont({
         {
             path: './FiraCode-Light.ttf',
             weight: '300',
-            display: 'swap',
         },
         {
             path: './FiraCode-Regular.ttf',
             weight: '400',
-            display: 'swap',
         },
         {
             path: './FiraCode-Medium.ttf',
             weight: '500',
-            display: 'swap',
         },
         {
             path: './FiraCode-SemiBold.ttf',
             weight: '600',
-            display: 'swap',
         },
         {
             path: './FiraCode-Bold.ttf',
             weight: '700',
-            display: 'swap',
         },
     ],
 });
